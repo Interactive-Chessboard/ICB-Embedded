0c6d891d54c5a09152608ce1ee840adf86f252ca
Christophe <christophe.lanouette@gmail.com>
2025-09-24 16:44:51 -0400

    Removed comments from main

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,7 @@
 
 void setup() 
 {
-  // Set up a serial connection
   Serial.begin(115200);
-  // Start the bluetooth server
   Bluetooth::init();
   //delay(8000); //for debugging purposes
 }
@@ -15,18 +13,15 @@ void setup()
 void loop()
 {
   // ---getGameSettings---
-  // Get game settings through the screen and buttons
   Settings game_settings = Board::getGameSettings(); //REMOVE COMMENT WHEN IMPLEMENTED
 
   // ---Bluetooth Connect---
-  // Confirm the bluetooth connection if playing online
   if (game_settings.game_mode == GameMode::Online && !Bluetooth::confirmConnection())
   {
     return;
   }
 
   // ---Init---
-  // Waits until all pieces are placed in their starting position
   int timeout_reached = Board::waitForInitBoard(6000); //REMOVE COMMENT WHEN IMPLEMENTED
   if (timeout_reached)
   {
@@ -34,12 +29,10 @@ void loop()
   }
 
   // ---Web App Settings---
-  // Get game settings from the web app if playing online
   if (game_settings.game_mode == GameMode::Online)
   {
     game_settings = Bluetooth::getWebAppSettings(); //REMOVE COMMENT WHEN IMPLEMENTED
   }
-
 
   // ---Start Game---
   Game::game(game_settings);
